1a824654641e5bb130e07656812c3a5282b80329
Nicole Adams <NicoleAdams.sci@gmail.com>
2025-11-14 16:46:28 -0500

    so many fixes

--- code/analyze_lr_outputs.R
+++ code/analyze_lr_outputs.R
@@ -21,7 +21,7 @@ dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
 cat("Output directory:", output_dir, "\n")
 
 # Define populations and relationships
-populations <- c("AfAm", "Cauc", "Hispanic", "Asian")
+populations <- c("AfAm", "Cauc", "Hispanic", "Asian", "all")
 relationships <- c("parent_child", "full_siblings", "half_siblings", 
                   "cousins", "second_cousins", "unrelated")
 
@@ -51,6 +51,9 @@ gc()
 # make sure combined_LR is numeric
 all_combined <- all_combined %>% mutate(combined_LR = as.numeric(combined_LR))
 
+# save all_combined
+all_combined_file <- file.path(output_dir, "combined_LR_all.rds")
+saveRDS(all_combined, all_combined_file, compress = "gzip")
 
 # 1. Define the data frame for Population Match ONLY (used for summary stats and proportions)
 # This data frame includes ALL tested relationships, as long as the population is correct.
@@ -59,7 +62,7 @@ all_combined_pop_match <- all_combined %>% filter(is_correct_pop == TRUE)
 # 2. Define the data frame for STRICT Match (used for the raw LR distribution plots)
 all_combined_strict_match <- all_combined_pop_match %>% filter(known_relationship == tested_relationship) %>% mutate(unique_pair_id = str_c(batch_id, pair_id, sep = "_"))
 
-# 3. Define the data frame for Population MISMATCH (used for ratio analysis)
+# 3. Define the data frame for Population MISMATCH (used for ratio analysis), relationship is known
 all_combined_mismatch <- all_combined %>% filter(is_correct_pop == FALSE)
 
 # 4. Save the combined LR files (Uses the STRICT match data)
@@ -77,14 +80,9 @@ summary_stats_file <- file.path(output_dir, "combined_LR_summary_stats.csv")
 fwrite(summary_stats, summary_stats_file)
 
 # Uses FULL combined data
-ratio_summary <- calculate_ratio_stats(all_combined)
+ratios <- calculate_ratio_stats(all_combined)
 ratio_summary_file <- file.path(output_dir, "combined_LR_ratio_summary.csv")
-fwrite(ratio_summary, ratio_summary_file)
+fwrite(ratios$ratio_summary, ratio_summary_file)
 
-cutoffs <- calculate_cutoffs(all_combined, c(1, 0.1, 0.01))
-cutoffs_file <- file.path(output_dir, "combined_LR_cutoffs.csv")
-fwrite(cutoffs, cutoffs_file)
-
-proportions_exceeding_cutoffs <- calculate_proportions_exceeding_cutoffs(all_combined, cutoffs)
-proportions_file <- file.path(output_dir, "combined_LR_exceeding_cutoffs.csv")
-fwrite(proportions_exceeding_cutoffs, proportions_file)
+ratios_raw_file <- file.path(output_dir, "combined_LR_ratios_raw.csv")
+fwrite(ratios$combined_lrs_ratio, ratios_raw_file)
